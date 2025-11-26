dce8e231aeacfd214bef5ed2129bb88c4b26e09a
sammy-ette <torchedsammy@gmail.com>
2025-11-26 09:15:38 -0400

    add sample data

---
+++ sql/employees.sql
@@ -0,0 +1,5 @@
+insert into `employees` (`address`, `id`, `name`, `salary`) values ('23 Lagoon Road', 2, 'Amy Schumer', 8000);
+insert into `employees` (`address`, `id`, `name`, `salary`) values ('67 Cherry Lane', 3, 'Jean Doe', 9905);
+insert into `employees` (`address`, `id`, `name`, `salary`) values ('1 Berry Road', 4, 'Carol Jane', 7000);
+insert into `employees` (`address`, `id`, `name`, `salary`) values ('137 Gravel Road', 5, 'Alen Gray', 11000);
+insert into `employees` (`address`, `id`, `name`, `salary`) values ('7th Street', 6, 'Stan Wilson', 8800);
